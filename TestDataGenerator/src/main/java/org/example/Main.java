b5194493658924aa05e33bc82ac08f64d9117f83
Sergiu Postolachi <postolachi.sergiu@gmail.com>
2022-11-03 13:44:50 +0200

    Create modules

--- TestDataGenerator/src/main/java/org/example/Main.java
+++ TestDataGenerator/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi world!");
+        System.out.println("Hi again world!");
     }
 }
